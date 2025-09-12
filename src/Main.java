a1750bba9a3c469bac42262de4c0ea368a0f4930
evelinabublyk <evelinabublykedu@gmail.com>
2025-09-12 14:46:11 +0200

    task 1 done

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,41 @@ public class Main {
         while(true) {
             System.out.println("Enter a number from 1 to 5");
             int task = sc.nextInt();
-
-            if (task == 0){
+            if (task < 0){
+                System.out.println("ERROR: invalid input!");
+            }
+            else if (task == 0){
                 System.out.println("Program finished");
                 break;
             }
             else if (task > 0 && task <= 5){
                 switch (task){
                     case 1:
+                        System.out.println("What is your goal-distance in km?");
+                        int goal = sc.nextInt();
+
+                        if (goal < 0) {
+                            System.out.println("ERROR: invalid input!");
+                            break;
+                        }
+
+                        System.out.println("How many kms can you ride on a first day");
+                        int firstDay = sc.nextInt();
+
+                        if (firstDay < 0) {
+                            System.out.println("ERROR: invalid input!");
+                            break;
+                        }
+
+                        int days = 0;
+                        double current = firstDay;
+
+                        while (current < goal) {
+                            current += current * 0.05;
+                            days++;
+                        }
+                        System.out.println("You need " + days + " days to achieve your goal-distance");
+                        break;
                     case 2:
                     case 3:
                     case 4:
